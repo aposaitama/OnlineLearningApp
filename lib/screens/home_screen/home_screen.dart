0e2f9daacd2b92748e9edd644416ae6b06033cb5
aposaitama <gist@MacBook-Pro-Gist.local>
2025-04-20 17:29:17 +0300

    update meetup widget

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:online_app/resources/app_colors.dart';
 import 'package:online_app/resources/app_fonts.dart';
+import 'package:online_app/screens/home_screen/widgets/meetup_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -50,58 +51,7 @@ class HomeScreen extends StatelessWidget {
         ),
         child: Column(
           children: [
-            Container(
-              height: 120.0,
-              decoration: BoxDecoration(
-                color: AppColors.lightPurpleColor,
-                borderRadius: BorderRadius.circular(
-                  15.0,
-                ),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        top: 25.0,
-                        left: 22.0,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Meetup',
-                            style: AppFonts.poppinsMedium.copyWith(
-                              color: AppColors.darkPurpleColor,
-                              fontSize: 24.0,
-                            ),
-                          ),
-                          Text(
-                            'Off-line exchange of learning experiences',
-                            style: AppFonts.poppinsRegular.copyWith(
-                              color: AppColors.darkPurpleColor,
-                              fontSize: 12.0,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10.0,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      right: 13.0,
-                    ),
-                    child: SvgPicture.asset(
-                      fit: BoxFit.scaleDown,
-                      'assets/icons/Meetup_icon.svg',
-                    ),
-                  )
-                ],
-              ),
-            )
+            MeetupWidget(),
           ],
         ),
       ),
---
+++ lib/screens/home_screen/widgets/meetup_widget.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:online_app/resources/app_colors.dart';
+import 'package:online_app/resources/app_fonts.dart';
+
+class MeetupWidget extends StatelessWidget {
+  const MeetupWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120.0,
+      decoration: BoxDecoration(
+          color: AppColors.lightPurpleColor,
+          borderRadius: BorderRadius.circular(
+            15.0,
+          ),
+          border: Border.all(
+            color: AppColors.lightPurpleColor,
+            width: 0.5,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.darkHintTextColor.withValues(
+                alpha: 0.1,
+              ),
+              spreadRadius: 5,
+              blurRadius: 5,
+              offset: const Offset(2, 5),
+            ),
+          ]),
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 25.0,
+                left: 22.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Meetup',
+                    style: AppFonts.poppinsMedium.copyWith(
+                      color: AppColors.darkPurpleColor,
+                      fontSize: 24.0,
+                    ),
+                  ),
+                  Text(
+                    'Off-line exchange of learning experiences',
+                    style: AppFonts.poppinsRegular.copyWith(
+                      color: AppColors.darkPurpleColor,
+                      fontSize: 12.0,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 13.0,
+            ),
+            child: SvgPicture.asset(
+              fit: BoxFit.scaleDown,
+              'assets/icons/Meetup_icon.svg',
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
