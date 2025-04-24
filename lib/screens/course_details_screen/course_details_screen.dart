8ae795625f6693ee3897c5ed08197105c4005173
aposaitama <gist@MacBook-Pro-Gist.local>
2025-04-24 12:15:15 +0300

    updated detailedCourseScreen

--- lib/screens/course_details_screen/course_details_screen.dart
+++ lib/screens/course_details_screen/course_details_screen.dart
@@ -9,7 +9,10 @@ import 'package:online_app/screens/course_details_screen/bloc/course_details_sta
 
 class CourseDetailsScreen extends StatefulWidget {
   final String courseId;
-  const CourseDetailsScreen({super.key, required this.courseId});
+  const CourseDetailsScreen({
+    super.key,
+    required this.courseId,
+  });
 
   @override
   State<CourseDetailsScreen> createState() => _CourseDetailsScreenState();
@@ -28,97 +31,141 @@ class _CourseDetailsScreenState extends State<CourseDetailsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final double imageHeight = MediaQuery.of(context).size.height / 3;
+    final double height = MediaQuery.of(context).size.height;
     return BlocBuilder<CourseDetailsBloc, CourseDetailsState>(
       builder: (context, state) {
         if (state.loadingStatus == CourseDetailsLoadingStatus.loaded) {
           final course = state.course;
           return Scaffold(
-            body: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                course != null
-                    ? SizedBox(
-                        height: 300.0,
-                        child: Image.network(
-                          fit: BoxFit.cover,
-                          'http://localhost:1337${course.courseImage.url}',
-                        ),
-                      )
-                    : Container(),
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          course?.courseTitle ?? '',
-                          style: AppFonts.poppinsBold.copyWith(
-                            color: Theme.of(context)
-                                .extension<AppColorsModel>()!
-                                .mainTextColor,
-                            fontSize: 20.0,
-                            height: 2.0,
-                          ),
-                        ),
-                        Text(
-                          '\$${course?.coursePrice.toString() ?? '0.0'}',
-                          style: AppFonts.poppinsBold.copyWith(
-                            color: AppColors.deepBlueColor,
-                            fontSize: 20.0,
-                            height: 2.0,
-                          ),
-                        )
-                      ],
+              body: Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              course != null
+                  ? SizedBox(
+                      width: double.infinity,
+                      height: imageHeight,
+                      child: Image.network(
+                        fit: BoxFit.cover,
+                        'http://localhost:1337${course.courseImage.url}',
+                      ),
                     )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(
-                      '6h 14min',
-                      style: AppFonts.poppinsRegular.copyWith(
+                  : Container(),
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: imageHeight - 22,
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
                         color: Theme.of(context)
                             .extension<AppColorsModel>()!
-                            .hintTextColor,
-                        fontSize: 12.0,
+                            .onSurface,
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(
+                            25.0,
+                          ),
+                          topRight: Radius.circular(
+                            25.0,
+                          ),
+                        ),
                       ),
+                      height: height - imageHeight + 22,
+                      width: double.infinity,
                     ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      '${course?.courseVideoItems.length.toString() ?? ''} Lessons',
-                      style: AppFonts.poppinsRegular.copyWith(
-                        color: Theme.of(context)
-                            .extension<AppColorsModel>()!
-                            .hintTextColor,
-                        fontSize: 12.0,
-                      ),
-                    ),
-                  ],
-                ),
-                Text(
-                  'About this course',
-                  style: AppFonts.poppinsBold.copyWith(
-                    color: Theme.of(context)
-                        .extension<AppColorsModel>()!
-                        .mainTextColor,
-                    fontSize: 16.0,
-                    height: 2.0,
                   ),
-                ),
-                Text(
-                  course?.courseDescription ?? '',
-                  style: AppFonts.poppinsRegular.copyWith(
-                    color: Theme.of(context)
-                        .extension<AppColorsModel>()!
-                        .hintTextColor,
-                    fontSize: 12.0,
-                  ),
-                ),
-              ],
-            ),
-          );
+                ],
+              )
+            ],
+          )
+              // Column(
+              //   crossAxisAlignment: CrossAxisAlignment.start,
+              //   children: [
+              //     course != null
+              //         ? SizedBox(
+              //             height: 300.0,
+              //             child: Image.network(
+              //               fit: BoxFit.cover,
+              //               'http://localhost:1337${course.courseImage.url}',
+              //             ),
+              //           )
+              //         : Container(),
+              //     Column(
+              //       children: [
+              //         Row(
+              //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //           children: [
+              //             Text(
+              //               course?.courseTitle ?? '',
+              //               style: AppFonts.poppinsBold.copyWith(
+              //                 color: Theme.of(context)
+              //                     .extension<AppColorsModel>()!
+              //                     .mainTextColor,
+              //                 fontSize: 20.0,
+              //                 height: 2.0,
+              //               ),
+              //             ),
+              //             Text(
+              //               '\$${course?.coursePrice.toString() ?? '0.0'}',
+              //               style: AppFonts.poppinsBold.copyWith(
+              //                 color: AppColors.deepBlueColor,
+              //                 fontSize: 20.0,
+              //                 height: 2.0,
+              //               ),
+              //             )
+              //           ],
+              //         )
+              //       ],
+              //     ),
+              //     Row(
+              //       children: [
+              //         Text(
+              //           '6h 14min',
+              //           style: AppFonts.poppinsRegular.copyWith(
+              //             color: Theme.of(context)
+              //                 .extension<AppColorsModel>()!
+              //                 .hintTextColor,
+              //             fontSize: 12.0,
+              //           ),
+              //         ),
+              //         const SizedBox(
+              //           width: 10.0,
+              //         ),
+              //         Text(
+              //           '${course?.courseVideoItems.length.toString() ?? ''} Lessons',
+              //           style: AppFonts.poppinsRegular.copyWith(
+              //             color: Theme.of(context)
+              //                 .extension<AppColorsModel>()!
+              //                 .hintTextColor,
+              //             fontSize: 12.0,
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //     Text(
+              //       'About this course',
+              //       style: AppFonts.poppinsBold.copyWith(
+              //         color: Theme.of(context)
+              //             .extension<AppColorsModel>()!
+              //             .mainTextColor,
+              //         fontSize: 16.0,
+              //         height: 2.0,
+              //       ),
+              //     ),
+              //     Text(
+              //       course?.courseDescription ?? '',
+              //       style: AppFonts.poppinsRegular.copyWith(
+              //         color: Theme.of(context)
+              //             .extension<AppColorsModel>()!
+              //             .hintTextColor,
+              //         fontSize: 12.0,
+              //       ),
+              //     ),
+              //   ],
+              // ),
+
+              );
         } else {
           return const SizedBox();
         }
